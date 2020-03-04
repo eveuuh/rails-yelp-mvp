608bd6e86e402200f58370cd5b8da55bf38b9404
eve jourdan <eve.jourdan@gmail.com>
2020-03-04 17:02:18 +0100

    not done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_102602) do
+ActiveRecord::Schema.define(version: 2020_03_04_104459) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
-    t.integer "phone"
+    t.string "address"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         ' Nems imperial',
+    address:      '1 rue de Rome 13006 Marseille',
+    category:     'chinese',
+    phone_number: '01 28 94 94 02',
+  },
+
+  {
+    name:         'Chez Mario',
+    address:      'Vieux port Marseille',
+    category:     'italian',
+    phone_number: '01 10 39 02 38',
+  },
+
+  {
+    name:         'Le yen',
+    address:      '100 avenue du prado Marseille',
+    category:     'japanese',
+    phone_number: '01 23 23 39 58',
+  },
+
+  {
+    name:         ' Loléas',
+    address:      'Cours Julien Marseille',
+    category:     'french',
+    phone_number: '01 10 23 34 50',
+  },
+
+  {
+    name:         ' La friterie ',
+    address:      'Castellane Marseille',
+    category:     'belgian',
+    phone_number: '01 20 93 34 50',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished Restaurant!'
+
+
+puts 'Creating reviews...'
+reviews_attributes = [
+  {
+    content:         ' good',
+    rating:      3,
+    restaurant_id: 1,
+  },
+ {
+    content:         'not good',
+    rating:      4,
+    restaurant_id: 1,
+  }
+]
+Review.create!(reviews_attributes)
+puts 'Finished Reviews!'
+
+
+puts 'Finished!'
